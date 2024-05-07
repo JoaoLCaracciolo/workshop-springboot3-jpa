b20f14a3409f13ce26c2c3b3e210b14052355380
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-05-07 15:31:29 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/caracciolo/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.caracciolo.course.repositories;
+
+import com.caracciolo.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
