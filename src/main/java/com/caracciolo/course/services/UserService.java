b4345746ca043dec58c54a944d4edf650a545d2a
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-05-08 16:10:50 -0300

    Exception handling - findById

--- src/main/java/com/caracciolo/course/services/UserService.java
+++ src/main/java/com/caracciolo/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.caracciolo.course.services;
 
 import com.caracciolo.course.entities.User;
 import com.caracciolo.course.repositories.UserRepository;
+import com.caracciolo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
